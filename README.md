cdfc940779cb3a6df5b9667424d3ea98cd14a52c
ichizero <30754019+ichizero@users.noreply.github.com>
2018-07-27 13:35:59 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# PoseNet for Qwatch
+
+This userscript detects person poses  by using Google PoseNet and draw it.
+
+## Usage
+Add user.js to Tampermonkey and set up match url for Qwatch
+
+If you want to change params, you can edit it.
---
+++ user.js
@@ -0,0 +1,240 @@
+// ==UserScript==
+// @name         PoseNet for Qwatch
+// @namespace    https://github.com/ichizero
+// @version      0.1
+// @description  This userscript detects person poses  by using Google PoseNet and draw it.
+// @author       ichizero
+// @require      https://unpkg.com/@tensorflow/tfjs
+// @require      https://unpkg.com/@tensorflow-models/posenet
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
+// ==/UserScript==
+
+var inline_src = (<><![CDATA[
+
+
+    const multiplier = 0.75 // powerful GPUs: 1.01, mid GPUs: 0.75, mobile: 0.5
+    posenet.load(multiplier).then((net) => detectPose(net));
+    
+    
+    function detectPose(net) {
+      // Params
+      const imageScaleFactor = 0.5; // A number between 0.2 and 1.0.
+      const flipHorizontal = false;
+      const outputStride = 16;      // Must be 32, 16, 8.
+      const maxPoseDetections = 5;
+      const minPoseConfidence = 0.3;
+      const minPartConfidence = 0.3;
+      const imageElementId = "MJPEG_streaming"
+    
+    
+      // Set up Canvas
+      const imageElement = document.getElementById(imageElementId);
+      imageElement.style.zIndex = 1;
+      imageElement.style.position = "absolute";
+    
+      const canvas = document.createElement("canvas");
+      canvas.width = imageElement.width;
+      canvas.height = imageElement.height;
+      canvas.style.zIndex = 2;
+      canvas.style.position = "absolute";
+    
+      const liveView = document.getElementById('liveView');
+      liveView.appendChild(canvas);
+    
+      const ctx = canvas.getContext('2d');
+    
+    
+      // Pose detection
+      async function poseDetectionFrame() {
+        const poses = await net.estimateMultiplePoses(imageElement, imageScaleFactor,
+          flipHorizontal, outputStride, maxPoseDetections);
+    
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        poses.forEach(({ score, keypoints }) => {
+          if (score >= minPoseConfidence) {
+            drawKeypoints(keypoints, minPartConfidence, ctx);
+            drawSkeleton(keypoints, minPartConfidence, ctx);
+          }
+        });
+    
+        requestAnimationFrame(poseDetectionFrame);
+      }
+    
+      poseDetectionFrame();
+    }
+    
+    
+    /**
+     * The following code was obtained from
+     * https://github.com/tensorflow/tfjs-models/blob/master/posenet/demos/demo_util.js
+     * and I removed "export" expression.
+     *
+     * Copyright 2018 Google Inc. All Rights Reserved.
+     * Licensed under the Apache License, Version 2.0 (the "License");
+     */
+    const color = 'aqua';
+    const lineWidth = 2;
+    
+    function toTuple({ y, x }) {
+      return [y, x];
+    }
+    
+    function drawPoint(ctx, y, x, r, color) {
+      ctx.beginPath();
+      ctx.arc(x, y, r, 0, 2 * Math.PI);
+      ctx.fillStyle = color;
+      ctx.fill();
+    }
+    
+    /**
+     * Draws a line on a canvas, i.e. a joint
+     */
+    function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
+      ctx.beginPath();
+      ctx.moveTo(ax * scale, ay * scale);
+      ctx.lineTo(bx * scale, by * scale);
+      ctx.lineWidth = lineWidth;
+      ctx.strokeStyle = color;
+      ctx.stroke();
+    }
+    
+    /**
+     * Draws a pose skeleton by looking up all adjacent keypoints/joints
+     */
+    function drawSkeleton(keypoints, minConfidence, ctx, scale = 1) {
+      const adjacentKeyPoints = posenet.getAdjacentKeyPoints(
+        keypoints, minConfidence);
+    
+      adjacentKeyPoints.forEach((keypoints) => {
+        drawSegment(toTuple(keypoints[0].position),
+          toTuple(keypoints[1].position), color, scale, ctx);
+      });
+    }
+    
+    /**
+     * Draw pose keypoints onto a canvas
+     */
+    function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
+      for (let i = 0; i < keypoints.length; i++) {
+        const keypoint = keypoints[i];
+    
+        if (keypoint.score < minConfidence) {
+          continue;
+        }
+    
+        const { y, x } = keypoint.position;
+        drawPoint(ctx, y * scale, x * scale, 3, color);
+      }
+    }
+    
+    /**
+     * Draw the bounding box of a pose. For example, for a whole person standing
+     * in an image, the bounding box will begin at the nose and extend to one of
+     * ankles
+     */
+    function drawBoundingBox(keypoints, ctx) {
+      const boundingBox = posenet.getBoundingBox(keypoints);
+    
+      ctx.rect(boundingBox.minX, boundingBox.minY,
+        boundingBox.maxX - boundingBox.minX, boundingBox.maxY - boundingBox.minY);
+    
+      ctx.stroke();
+    }
+    
+    /**
+     * Converts an arary of pixel data into an ImageData object
+     */
+    async function renderToCanvas(a, ctx) {
+      const [height, width] = a.shape;
+      const imageData = new ImageData(width, height);
+    
+      const data = await a.data();
+    
+      for (let i = 0; i < height * width; ++i) {
+        const j = i * 4;
+        const k = i * 3;
+    
+        imageData.data[j + 0] = data[k + 0];
+        imageData.data[j + 1] = data[k + 1];
+        imageData.data[j + 2] = data[k + 2];
+        imageData.data[j + 3] = 255;
+      }
+    
+      ctx.putImageData(imageData, 0, 0);
+    }
+    
+    /**
+     * Draw an image on a canvas
+     */
+    function renderImageToCanvas(image, size, canvas) {
+      canvas.width = size[0];
+      canvas.height = size[1];
+      const ctx = canvas.getContext('2d');
+    
+      ctx.drawImage(image, 0, 0);
+    }
+    
+    /**
+     * Draw heatmap values, one of the model outputs, on to the canvas
+     * Read our blog post for a description of PoseNet's heatmap outputs
+     * https://medium.com/tensorflow/real-time-human-pose-estimation-in-the-browser-with-tensorflow-js-7dd0bc881cd5
+     */
+    function drawHeatMapValues(heatMapValues, outputStride, canvas) {
+      const ctx = canvas.getContext('2d');
+      const radius = 5;
+      const scaledValues = heatMapValues.mul(tf.scalar(outputStride, 'int32'));
+    
+      drawPoints(ctx, scaledValues, radius, color);
+    }
+    
+    /**
+     * Used by the drawHeatMapValues method to draw heatmap points on to
+     * the canvas
+     */
+    function drawPoints(ctx, points, radius, color) {
+      const data = points.buffer().values;
+    
+      for (let i = 0; i < data.length; i += 2) {
+        const pointY = data[i];
+        const pointX = data[i + 1];
+    
+        if (pointX !== 0 && pointY !== 0) {
+          ctx.beginPath();
+          ctx.arc(pointX, pointY, radius, 0, 2 * Math.PI);
+          ctx.fillStyle = color;
+          ctx.fill();
+        }
+      }
+    }
+    
+    /**
+     * Draw offset vector values, one of the model outputs, on to the canvas
+     * Read our blog post for a description of PoseNet's offset vector outputs
+     * https://medium.com/tensorflow/real-time-human-pose-estimation-in-the-browser-with-tensorflow-js-7dd0bc881cd5
+     */
+    function drawOffsetVectors(
+      heatMapValues, offsets, outputStride, scale = 1, ctx) {
+      const offsetPoints = posenet.singlePose.getOffsetPoints(
+        heatMapValues, outputStride, offsets);
+    
+      const heatmapData = heatMapValues.buffer().values;
+      const offsetPointsData = offsetPoints.buffer().values;
+    
+      for (let i = 0; i < heatmapData.length; i += 2) {
+        const heatmapY = heatmapData[i] * outputStride;
+        const heatmapX = heatmapData[i + 1] * outputStride;
+        const offsetPointY = offsetPointsData[i];
+        const offsetPointX = offsetPointsData[i + 1];
+    
+        drawSegment([heatmapY, heatmapX], [offsetPointY, offsetPointX],
+          color, scale, ctx);
+      }
+    }
+    
+    
+    
+    
+    ]]></>).toString();
+    var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
+    eval(c.code);
